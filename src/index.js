71cab0bace2987478aa1e3bbe2468e0a372cd507
Abhijeet <dev1.parsa@gmail.com>
2020-06-19 01:03:52 -0400

    nested component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className="ui comments">
+      <ApprovalCard>
       <CommentDetail author="Abhi" />
+      </ApprovalCard>
       <CommentDetail author="Sang" />
       <CommentDetail author="Vinni" />
     </div>
